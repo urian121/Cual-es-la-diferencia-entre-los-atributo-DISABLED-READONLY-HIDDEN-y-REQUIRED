cbd5a829b117afe0dff0bd8f03c19004ffc3395c
URIAN VIERA <urian1213viera@gmail.com>
2021-09-12 12:07:31 -0500

    Update dataForm.php
    Ultimo cambio.

--- dataForm.php
+++ dataForm.php
@@ -1,28 +1,17 @@
 <?php
-$nameFull = !empty($_REQUEST['nameFull']);
-$cedula   = isset($_REQUEST['cedula']);
-echo $_REQUEST['canal'];
+$nombre     = $_REQUEST['nombre'];
+$clave      = $_REQUEST['clave'];
+echo $codigo = $_REQUEST['codigo'];
 
+
+
+echo '<br>Nombre:  '. $nombre . ' Clave: ' .$clave;
+echo '<br><br><br><br>';
 
 foreach ($_POST as $i => $value)
 {
-  echo 'Valores Recibidos:' .$value.'<br>';
+  echo 'Valores Recibidos '. $i. ':' .$value.'<br>';
 } 
 
-
-
-//isset, Determina si una variable está definida y no es NULL
-echo $var = isset($_POST['nameFull']) ? $_POST['nameFull'] : $_GET['nameFull'];
-//$a = $cond ? true : false; 
-$miVar = (!empty( $_GET[ 'nameFull' ]) ? $_GET[ 'nameFull' ] : 'No hay Nada' );
-
-
-
-if(!empty($_GET[ 'nameFull' ])) {
-   echo 'caso 1:'. $_GET[ 'nameFull' ];
-}
-else {
-   echo $id = 'No hay nada';
-}
 
 ?>
